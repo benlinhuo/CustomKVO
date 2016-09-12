e3cbbf0642110c109325d3e10bfb0a70772ffb31
霍本林 <benlinhuo@angejia.com>
2016-09-12 20:11:06 +0800

    kvo

--- HBLCustomKVO/HBLCustomKVO/Observation1ViewController.m
+++ HBLCustomKVO/HBLCustomKVO/Observation1ViewController.m
@@ -10,6 +10,8 @@
 #import "NSObject+KVO.h"
 #import "HBLTestObservation.h"
 #import "Observation2ViewController.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
 
 
 @interface Observation1ViewController () {
@@ -30,22 +32,30 @@ static void *ktestObserv = &ktestObserv;
     [super viewDidLoad];
     
     testObservation = [HBLTestObservation shared];
-    [testObservation addHBLObserver:self forKey:@"userName" context:@"testObservation"];
+//    [testObservation addHBLObserver:self forKey:@"userName" context:@"testObservation"];
     
-//    [testObservation addObserver:self forKeyPath:@"userName" options:NSKeyValueObservingOptionNew context:ktestObserv];
+    [testObservation addObserver:self forKeyPath:@"userName" options:NSKeyValueObservingOptionNew context:ktestObserv];
 }
 
-//- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
-//{
-//    if (context == ktestObserv) {
-//        NSLog(@"same");
-//    }
-//    NSLog(@"sdfsdf");
-//
-//    NSLog(@"isa= %s", object_getClassName(object)); // 获取 isa 指针所指向的类
-//
-//    NSLog(@"class=%@", [object class]);
-//}
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    
+    if (context == ktestObserv) {
+        NSLog(@"same");
+    }
+    NSLog(@"对象 testObservation 的属性 userName 发生变化啦！");
+
+    NSLog(@"isa= %s", object_getClassName(object)); // 获取 isa 指针所指向的类
+
+    NSLog(@"class=%@", [object class]);
+    
+    NSLog(@"superClass = %@", class_getSuperclass(object_getClass(object)));
+}
+
+- (void)dealloc
+{
+    [testObservation removeObserver:self forKeyPath:@"userName"];
+}
 
 
 - (void)observerHBLValueForKey:(NSString *)key ofObject:(NSObject *)object newValue:(id)newValue oldValue:(id)oldValue context:(NSString *)context
